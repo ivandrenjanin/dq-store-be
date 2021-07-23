f072505c9d69c9985d2d7f9ad33ecaf0791d7033
Ivan Drenjanin <drenjanin.i@gmail.com>
2021-07-23 20:50:55 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api');
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('D/Q Storehouse')
