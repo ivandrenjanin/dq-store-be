b2d6dec930ff9424076beac3a0a3707da50da6a9
Ivan Drenjanin <drenjanin.i@gmail.com>
2022-01-28 13:58:32 +0100

    fix company client

--- src/modules/company-client/dto/create-company-client.dto.ts
+++ src/modules/company-client/dto/create-company-client.dto.ts
@@ -1,17 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateCompanyClientDto {
   @ApiProperty({
-    minLength: 1,
-    maxLength: 100,
     required: true,
   })
   @IsNotEmpty()
@@ -19,112 +10,79 @@ export class CreateCompanyClientDto {
   public name!: string;
 
   @ApiProperty({
-    minLength: 1,
-    maxLength: 100,
     required: false,
   })
   @IsOptional()
-  @IsNotEmpty()
   @IsString()
   public postalCode?: string;
 
   @ApiProperty({
-    minLength: 1,
-    maxLength: 100,
     required: false,
   })
   @IsOptional()
-  @IsNotEmpty()
   @IsString()
   public streetNumber?: string;
 
   @ApiProperty({
-    minLength: 1,
-    maxLength: 100,
     required: false,
   })
   @IsOptional()
-  @IsNotEmpty()
   @IsString()
   public street?: string;
 
   @ApiProperty({
-    minLength: 1,
-    maxLength: 100,
     required: false,
   })
   @IsOptional()
-  @IsNotEmpty()
   @IsString()
   public city?: string;
 
   @ApiProperty({
-    minLength: 1,
-    maxLength: 100,
     required: false,
   })
   @IsOptional()
-  @IsNotEmpty()
   @IsString()
   public taxIdNumber?: string;
 
   @ApiProperty({
-    minLength: 1,
-    maxLength: 100,
     required: false,
   })
   @IsOptional()
-  @IsNotEmpty()
   @IsString()
   public companyNumber?: string;
 
   @ApiProperty({
-    minLength: 1,
-    maxLength: 100,
     required: false,
   })
   @IsOptional()
-  @IsNotEmpty()
   @IsString()
   public bankName?: string;
 
   @ApiProperty({
-    minLength: 1,
-    maxLength: 100,
     required: false,
   })
   @IsOptional()
-  @IsNotEmpty()
   @IsString()
   public bankAccountNumber?: string;
 
   @ApiProperty({
-    minLength: 1,
-    maxLength: 100,
     required: false,
   })
   @IsOptional()
-  @IsNotEmpty()
   @IsString()
   public phoneFaxNumber?: string;
 
   @ApiProperty({
-    minLength: 1,
-    maxLength: 100,
     required: false,
   })
   @IsOptional()
-  @IsNotEmpty()
   @IsString()
   public phoneMobileNumber?: string;
 
   @ApiProperty({
-    minLength: 1,
-    maxLength: 100,
     required: false,
   })
   @IsOptional()
-  @IsNotEmpty()
   @IsString()
   @IsEmail()
   public email?: string;
